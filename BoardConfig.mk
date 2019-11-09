20f000a031e203bf6ba7d72d074ff5a6e4c8e8af
Nolen Johnson <johnsonnolen@gmail.com>
2019-11-09 23:02:36 +0000

    yellowstone: Import various configurations
    * Move everything to vendor that can be.
    
    * The following were kanged:
     * bdroid_buildcfg.h was taken from shieldtablet as it has the same
       Broadcom Bluetooth module. Adapted the header for our device name.
    
    * The following were pulled from Stock:
     * audio_policy.conf is used by the Audio HAL.
     * bt_vendor.conf is used by the BT HAL, move firmware to vendor.
     * enctune.conf is used by the Audio HAL.
     * keylayout is used by the Raydium touchscreen.
     * models_frontal.xml is used by the Camera HAL.
     * nfc* - Updated the namespace of nfcee_access to avoid build errors,
       renamed secondary nfc configuration from original name to reflect
       the correct HAL (was bcm2907{b03}, whereas our chip reports as
       bcm43342{b00}.
     * nvcamera.conf is used by the Camera HAL.
    
    * The following were pulled from tn8:
     * nvaudio_conf.xml was partially kanged from tn8 and updated for
       yellowstone by oscardagrach.
      * oscardagrach: This is about 90% ported from stock to the new
        format. Most everything should be working so far. The old
        config was a kang of shieldtablet that only partially worked
        out of coincidence.
     * gps* - Are used by the GPS HAL.
     * media* - used by codecs, etc.
      * We've finally fixed hardware OMX for t124 SoC. The issue
        was unresolved symbols from deprecated hardfloat/FPU functions.
        While not an ideal solution, it works now.
     * seccomp* - used by media codecs, etc.
    
    * Opted to include Google media codecs/codecs_performance/profiles as well,
      as this is technically a Google-made device.
    
    * Import the common Broadcom Wi-Fi configuration and edit for our needs.
    
    * Set relevant Bluetooth/Wi-Fi Board Flags. The basic setup was taken
      from shieldtablet, as it has the same Broadcom Bluetooth/Wi-Fi modules.
    
    Signed-off-by: Nolen Johnson <johnsonnolen@gmail.com>
    Change-Id: I8b78b658ca61484e6534ac10692f120f9db07556
    
    Co-authored-by: Nolen Johnson <johnsonnolen@gmail.com>
    Co-authored-by: Vitaliy Tomin <highwaystar.ru@gmail.com>
    Co-authored-by: Ryan Grachek <ryan@edited.us>
    Change-Id: Ic351595ad98942ebbac69738422abbe3abe5f9b8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,6 +25,11 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a15
 
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+
 # Board
 TARGET_BOARD_PLATFORM := tegra
 TARGET_NO_BOOTLOADER := true
@@ -41,6 +46,23 @@ BOARD_FLASH_BLOCK_SIZE := 4096
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2097152000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 119358357504
+
+# Seccomp
+BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp
+
+# Wi-Fi
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
+BOARD_WLAN_DEVICE                := bcmdhd
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+WIFI_DRIVER_FW_PATH_STA          := "/system/vendor/firmware/bcm43341/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_AP           := "/system/vendor/firmware/bcm43341/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_P2P          := "/system/vendor/firmware/bcm43341/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_MODULE_ARG           := "iface_name=wlan0"
+WIFI_DRIVER_MODULE_NAME          := "bcmdhd"
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
 
 # Inherit from the proprietary board configuration
 include vendor/google/yellowstone/BoardConfigVendor.mk
--- device.mk
+++ device.mk
@@ -12,11 +12,49 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/audio/enctune.conf:system/etc/enctune.conf \
+    $(LOCAL_PATH)/audio/nvaudio_conf.xml:system/etc/nvaudio_conf.xml
+
+# Bluetooth
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
+
+# Camera
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/camera/model_frontal.xml:system/etc/model_frontal.xml \
+    $(LOCAL_PATH)/camera/nvcamera.conf:system/etc/nvcamera.conf
+
+# Codecs
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
+    $(LOCAL_PATH)/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
+    $(LOCAL_PATH)/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
+    $(LOCAL_PATH)/media/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml
+
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
+# GPS
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps/gps.conf \
+    $(LOCAL_PATH)/gps/gpsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/gps/gpsconfig.xml
+
 # HWUI
-$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
+
+# Keylayouts
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayouts/raydium_ts.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/raydium_ts.idc
+
+# NFC
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
+    $(LOCAL_PATH)/nfc/nfcee_access.xml:system/etc/nfcee_access.xml
 
 # RemovePackages
 PRODUCT_PACKAGES += \
@@ -24,3 +62,8 @@ PRODUCT_PACKAGES += \
 
 # System Properties
 -include $(LOCAL_PATH)/system_prop.mk
+
+# Wi-Fi
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
+

781045ba8fa137ab74f90005c09ea3723578e6fa
Nolen Johnson <johnsonnolen@gmail.com>
2020-02-17 22:57:18 -0500

    yellowstone: Migrate from bt_vendor.conf to vnd_yellowstone.txt
    * Bluetooth HAL now expects VND configuration input instead.
    * Comments are not supported in vnd_*.txt, so remove them.
    
    Change-Id: Ie46b3688599367b23d5b47b234adb2a39e6c69f5

--- device.mk
+++ device.mk
@@ -46,9 +46,6 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
     libbt-vendor
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
-
 # Camera
 PRODUCT_PACKAGES += \
     Snap

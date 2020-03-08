f37bfcb98576264d253df6a977f5ab260008de8b
Alessandro Astone <ales.astone@gmail.com>
2020-03-07 23:13:27 -0500

    yellowstone: Address 'LOCAL_MODULE_TAGS := eng debug' deprecation.
    * Newer AOSP versions deprecate LOCAL_MODULE_TAGS := eng debug.
      We now remove that and instead add the package to
      PRODUCT_PACKAGES_ENG or PRODUCT_PACKAGES_DEBUG as applicable.
    
    * To ensure that we don't miss out on any package during this switch,
      we add these explicitly, even though it might have been already
      added under PRODUCT_PACKAGES at some other location.
    
    Change-Id: I35bd67c3e7752be5bcd8f8682864062173e92724

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.yellowstone
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := fstab.yellowstone
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/etc
@@ -9,7 +9,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.comms.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.comms.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/etc/init/hw
@@ -17,7 +17,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.icera.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.icera.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/etc/init/hw
@@ -25,7 +25,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.icera.common.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.icera.common.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/etc/init/hw
@@ -33,7 +33,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.icera.tablet.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.icera.tablet.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/etc/init/hw
@@ -41,7 +41,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.nv_dev_board.usb.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.nv_dev_board.usb.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/etc/init/hw
@@ -49,7 +49,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.t124.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.t124.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/etc/init/hw
@@ -57,7 +57,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.tlk.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.tlk.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/etc/init/hw
@@ -65,7 +65,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.yellowstone.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.yellowstone.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/etc/init/hw
@@ -73,7 +73,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.ussrd.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.ussrd.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/etc/init/hw
@@ -81,7 +81,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := ussrd.conf
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := ussrd.$(TARGET_TEGRA_VERSION).conf
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/etc
@@ -100,7 +100,7 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.yellowstone.rc
 LOCAL_MODULE_STEM  := ueventd.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := ueventd.yellowstone.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)

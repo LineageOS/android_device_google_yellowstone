8b28c8f4d73950cf3bc771fe03eb4ff08e2e227b
Nolen Johnson <johnsonnolen@gmail.com>
2020-04-13 18:37:25 -0400

    yellowstone: Kang LMKD configuration from bonito
    Change-Id: I070b1d194da5f340affe2e807390e956f151cc57

--- system_prop.mk
+++ system_prop.mk
@@ -40,6 +40,18 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196610 \
     ro.zygote.disable_gl_preload=true
 
+# lmkd
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.lmk.low=1001 \
+    ro.lmk.medium=800 \
+    ro.lmk.critical=0 \
+    ro.lmk.critical_upgrade=false \
+    ro.lmk.upgrade_pressure=100 \
+    ro.lmk.downgrade_pressure=100 \
+    ro.lmk.kill_heaviest_task=true \
+    ro.lmk.kill_timeout_ms=100 \
+    ro.lmk.use_minfree_levels=true
+
 # LTE, CDMA, GSM/WCDMA
 PRODUCT_PROPERTY_OVERRIDES += \
     keyguard.no_require_sim=true \
